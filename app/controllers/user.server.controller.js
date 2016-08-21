ed96cf5302d0446e8f7ac4dd8a8762f46d0e36d1
caim03 <christian.lariccia@gmail.com>
2016-08-22 01:26:39 +0200

    New user CRUD operation

--- app/controllers/user.server.controller.js
+++ app/controllers/user.server.controller.js
@@ -1,3 +1,5 @@
+/* */
+
 /* Import user schema  */
 var User = require('../models/user.server.model');
 
@@ -6,9 +8,69 @@ exports.getAll = function(req, res){
     User.find(function(err, users){
         if(err){
 	    res.send(err);
-	}
-	else{
-	   res.json(users);
-	}
+		}
+		else{
+			res.json(users);
+		}
     });
 }
+
+exports.addUser = function(req, res){
+	/* Add a user to database, create a new user and pass it a json, retrieve from client */
+	var user = new User(req.body);
+	user.save(function(err){
+		if(err){
+			res.send(err);
+		}
+		else{
+			res.json(user);
+		}
+	});
+}
+
+/* Use read function to send single user, but read is performed by getUserById */
+exports.read = function(req, res){
+	res.json(req.user);
+}
+
+/* Use getUserById in app.param to pass id as a parameter */
+exports.getUserById = function(req, res, next, id){
+	/* Retrieve a single user using him id */
+	User.findOne({_id: id}, function(err, user){
+		if(err){
+			res.send(err);
+		}
+		else{
+			req.user = user; // Create user property in request object
+			next(); // Call read function as a next middleware
+		}
+	});
+}
+
+exports.deleteUser = function(req, res){
+	/* Use app.param('userId', getUserById) to retrive and delete an user
+	 * It calls getUserById and obtains the right user; after it completes the delete request
+	 * and sends the user in request object (req.user is our user) */
+	req.user.remove(function(err){
+		if(err){
+			res.send(err);
+		}
+		else {
+			res.json(req.user);
+		}
+	});
+}
+
+exports.updateUser = function(req, res){
+	/* req.user.id is the id of user that we update 
+	 * req.body contain the json with new information */
+	User.findByIdAndUpdate(req.user.id, req.body, function(err, user){
+		if(err){
+			res.send(err);
+		}
+		else{
+			res.json(user);
+		}
+	});
+}
+
--- app/routes/user.server.routes.js
+++ app/routes/user.server.routes.js
@@ -2,5 +2,15 @@
 var userCtrl = require('../controllers/user.server.controller');
 
 module.exports = function(app){
-    app.get('/api/users', userCtrl.getAll);
+    app.get('/users', userCtrl.getAll);
+    app.post('/users', userCtrl.addUser);
+    
+    app.get('/users/:userId', userCtrl.read);
+    app.delete('/users/:userId', userCtrl.deleteUser);
+    app.put('/users/:userId', userCtrl.updateUser);
+    
+    /* Pass the user id to getUserById function 
+     * This route for param is used everytime we use a url like /users/:userId 
+     * It is used in findOne, remove, update methods */
+    app.param('userId', userCtrl.getUserById);
 }
